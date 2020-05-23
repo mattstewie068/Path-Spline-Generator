e25761b8f62c84af148d9ab8e68a173123dd8778
Matthew <mattstewie068@gmail.com>
2020-05-22 23:13:14 -0400

    Working iteration of when to shoot and not shoot

--- Coordinate_conversion.py
+++ Coordinate_conversion.py
@@ -3,7 +3,7 @@ import math
 
 def weirdzero(x, y):
     if x != 0:
-        fraction = y/x
+        fraction = y / x
         # angle = weird_division(num, den)*180/math.pi
         newangle = math.atan(fraction) * 180 / math.pi
         if x < 0:
@@ -18,17 +18,27 @@ def weirdzero(x, y):
     return newangle
 
 
+def degree_to_rad(rad_angle):
+    """input angle in degree, output angle in radian"""
+    return (rad_angle * math.pi / 180)
+
+
+def rad_to_degree(deg_angle):
+    """input angle in radian, output angle in degree"""
+    return (deg_angle * 180 / math.pi)
+
+
 def convert_p_to_c(position, radius, angle):
     """converts a given point from polar to cartesian with angle in degrees"""
-    x = radius * math.cos((angle * math.pi / 180))
-    y = radius * math.sin((angle * math.pi / 180))
-    output = [x+position[0], y+position[1]]
+    x = radius * math.cos(degree_to_rad(angle))
+    y = radius * math.sin(degree_to_rad(angle))
+    output = [x + position[0], y + position[1]]
     return output
 
 
 def convert_c_to_p(position):
     """converts a given point from cartesian to polar with angle in degrees"""
-    r = math.sqrt((position[0])**2+(position[1]**2))
+    r = math.sqrt((position[0])**2 + (position[1]**2))
     angle = weirdzero(position[0], position[1])
     output = [r, angle]
     return output
--- goal_angle_compensation_visualizer.py
+++ goal_angle_compensation_visualizer.py
@@ -1,6 +1,8 @@
 import pygame
 import Color_List as CL
 import My_Functions
+import math
+import Coordinate_conversion as cc
 
 pygame.init()
 
@@ -11,19 +13,20 @@ WINDOWSIZE = [SCREENWIDTH, SCREENHEIGHT]
 zoom_factor = 3
 
 TOP_TO_GOAL = 30
-OUTER_GOAL_WIDTH = 36
-INNER_GOAL_WIDTH = 13
+OUTER_GOAL_WIDTH = 36 * zoom_factor
+INNER_GOAL_WIDTH = 13 * zoom_factor
 GOAL_DEPTH = 29
-BALL_SIZE = 7
+BALL_SIZE = 7 * zoom_factor
 
 OUTER_GOAL_CENTER = [int(SCREENWIDTH / 2), (TOP_TO_GOAL + GOAL_DEPTH)]
+INNER_GOAL_CENTER = [int(SCREENWIDTH / 2), TOP_TO_GOAL]
 
 # setting up inner goal size
-inner_start_pos = int((SCREENWIDTH / 2) - (0.5 * INNER_GOAL_WIDTH * zoom_factor))
-inner_end_pos = int(inner_start_pos + INNER_GOAL_WIDTH * zoom_factor)
+inner_start_pos = int((SCREENWIDTH / 2) - (0.5 * INNER_GOAL_WIDTH))
+inner_end_pos = int(inner_start_pos + INNER_GOAL_WIDTH)
 
-outer_start_pos = int((SCREENWIDTH / 2) - ((OUTER_GOAL_WIDTH / 2) * zoom_factor))
-outer_end_pos = int(outer_start_pos + OUTER_GOAL_WIDTH * zoom_factor)
+outer_start_pos = int((SCREENWIDTH / 2) - ((OUTER_GOAL_WIDTH / 2)))
+outer_end_pos = int(outer_start_pos + OUTER_GOAL_WIDTH)
 
 
 def Draw_Goals():
@@ -32,17 +35,17 @@ def Draw_Goals():
     pygame.draw.line(screen, CL.GREEN, (inner_start_pos, 0), (inner_start_pos, TOP_TO_GOAL), 3)  # draw inner goal
     pygame.draw.line(screen, CL.GREEN, (inner_end_pos, 0), (inner_end_pos, TOP_TO_GOAL), 3)  # draw inner goal
 
-    pygame.draw.line(screen, CL.RED, (outer_start_pos, TOP_TO_GOAL),
+    pygame.draw.line(screen, CL.WHITE, (outer_start_pos, TOP_TO_GOAL),
                      (inner_start_pos, TOP_TO_GOAL), 3)  # draw backboard
-    pygame.draw.line(screen, CL.RED, (inner_end_pos, TOP_TO_GOAL), (outer_end_pos, TOP_TO_GOAL), 3)  # draw backboard
+    pygame.draw.line(screen, CL.WHITE, (inner_end_pos, TOP_TO_GOAL), (outer_end_pos, TOP_TO_GOAL), 3)  # draw backboard
 
     pygame.draw.line(
-        screen, CL.RED, (outer_start_pos, TOP_TO_GOAL),
-        (outer_start_pos, TOP_TO_GOAL + GOAL_DEPTH),
+        screen, CL.WHITE, (outer_start_pos, TOP_TO_GOAL),
+        (outer_start_pos, TOP_TO_GOAL + (GOAL_DEPTH)),
         3)  # draw depth
     pygame.draw.line(
-        screen, CL.RED, (outer_end_pos, TOP_TO_GOAL),
-        (outer_end_pos, TOP_TO_GOAL + GOAL_DEPTH),
+        screen, CL.WHITE, (outer_end_pos, TOP_TO_GOAL),
+        (outer_end_pos, TOP_TO_GOAL + (GOAL_DEPTH)),
         3)  # draw depth
 
     pygame.draw.line(
@@ -52,8 +55,17 @@ def Draw_Goals():
     pygame.draw.line(screen, CL.WHITE, (outer_end_pos, TOP_TO_GOAL + GOAL_DEPTH),
                      (SCREENWIDTH, TOP_TO_GOAL + GOAL_DEPTH), 3)  # draw field wall
 
-def Inner_Angle():
-    pass
+
+def Triangle_solver(a, b, angle_c):
+    angle_c_rad = cc.degree_to_rad(angle_c)
+    c = math.sqrt(b**2 + a**2 - (2 * b * a * math.cos(angle_c_rad)))
+    angle_a_rad = math.acos((c**2 + b**2 - a**2) / (2 * c * b))
+    angle_a = cc.rad_to_degree(angle_a_rad)
+    return angle_a
+
+
+Triangle_solver(29, 144, 100)
+
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode(WINDOWSIZE)
@@ -69,16 +81,40 @@ while RUN:  # run until quit
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
             RUN = False
 
-    pygame.draw.circle(screen, CL.YELLOW, (int(SCREENWIDTH / 2), 15), int((BALL_SIZE * zoom_factor) / 2))
-
-    angle = My_Functions.cursor_angle_to_point(OUTER_GOAL_CENTER)
     mousepos = pygame.mouse.get_pos()
-    distance = My_Functions.distance(mousepos, OUTER_GOAL_CENTER)
-
     screen.fill(CL.BLACK)
     Draw_Goals()
-    pygame.draw.circle(screen, CL.YELLOW, mousepos, int((BALL_SIZE * zoom_factor) / 2))
-    pygame.draw.line(screen, CL.PURPLE, mousepos, OUTER_GOAL_CENTER, 3)  # draw line from mouse to outer goal center
+    pygame.draw.circle(screen, CL.YELLOW, (int(SCREENWIDTH / 2), 15), int((BALL_SIZE) / 2))
+
+    theta = (My_Functions.cursor_angle_to_point(OUTER_GOAL_CENTER) + 180) % 360
+    print(theta)
+
+    distance = My_Functions.distance(mousepos, OUTER_GOAL_CENTER)
+    # print(distance)
+
+    distance_to_inner = My_Functions.distance(mousepos, INNER_GOAL_CENTER)
+    # print(My_Functions.cursor_angle_to_point(OUTER_GOAL_CENTER))
+
+    if theta >= 180 and theta <= 360:
+        # draw line from mouse to outer goal center
+        pygame.draw.line(screen, CL.RED, mousepos, OUTER_GOAL_CENTER, 3)
+        pygame.draw.circle(screen, CL.YELLOW, mousepos, int((BALL_SIZE) / 2))
+
+    if theta >= 195 and theta <= 345:
+        pygame.draw.line(screen, CL.GREEN, mousepos, (cc.convert_p_to_c(mousepos, distance, theta)),
+                         3)  # draw line from mouse at angle and distance
+
+    if theta >= 220 and theta <= 270:
+        new_angle = Triangle_solver(GOAL_DEPTH, distance, theta + 90)
+        # print(new_angle)
+        pygame.draw.line(screen, CL.BLUE, mousepos, (cc.convert_p_to_c(
+            mousepos, distance_to_inner, new_angle + theta)), 3)
+
+    if theta >= 270 and theta <= 325:
+        new_angle = -Triangle_solver(GOAL_DEPTH, distance, theta + 90)
+        # print(new_angle)
+        pygame.draw.line(screen, CL.BLUE, mousepos, (cc.convert_p_to_c(
+            mousepos, distance_to_inner, new_angle + theta)), 3)
 
     pygame.display.flip()
 
